412cae9ef38b83e3a92aff2f9ecb9c168d50bf6a
= <=>
2025-03-20 14:42:11 -0300

    convert to my time zone -3

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import requests
 from datetime import datetime
 
 # Cianorte - Paraná location
-MY_LAT = -23.663349
-MY_LNG = -52.604439
+MY_LAT = -23.6633
+MY_LNG = -52.6055
+TIME_ZONE = -3
 
 parameters = {
     "lat": MY_LAT,
@@ -11,12 +12,17 @@ parameters = {
     "formatted": 0
 }
 
+
+
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise_utc = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset_utc = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise_time = datetime.strptime(sunrise_utc, "%H").hour + TIME_ZONE
+sunset_time = datetime.strptime(sunset_utc, "%H").hour + TIME_ZONE
+
 
 time_now = datetime.now()
 
-print(time_now.hour)
+
